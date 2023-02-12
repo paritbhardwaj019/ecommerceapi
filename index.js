5efb607d0a8941b51beb5ff96daa4ea56c004b5c
Parit Bhardwaj <111231393+paritbhardwaj019@users.noreply.github.com>
2023-02-12 12:13:36 +0530

    Update PORT Variable

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(cors());
 app.use("/api/v1/products", allRoutes);
 
-const PORT = process.env.SERVER_PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL || "";
 
 app.get("/", (request, response) => {
